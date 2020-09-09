fixed a bug where phot was averaged over too many dimensions, giving wrong tsys values

sim/tsysmeasure.py
@@ -81,7 +81,7 @@ class TsysMeasure:
                         max_idx_vane = np.argmin(np.abs(self.vane_times - tod_timesi[max_idxi]))
                         if max_idxi > min_idxi and max_idx_vane > min_idx_vane:
                             self.Thot[feed_idx, i] = np.nanmean(self.Thot_cont[min_idx_vane:max_idx_vane])
-                            self.Phot[feed_idx, :, :, i] = np.nanmean(todi[:,:,min_idxi:max_idxi])
+                            self.Phot[feed_idx, :, :, i] = np.nanmean(todi[:,:,min_idxi:max_idxi], axis=(2))
                             self.Phot_t[feed_idx, i] = (tod_timesi[min_idxi] + tod_timesi[max_idxi])/2.0
                             self.points_used[feed_idx] = max_idxi - min_idxi
 